5497a79e9afea4276bc04a5704ea54d4f0036cdc
ARVIND_VERMA <arvindverma630635@gmail.com>
2025-01-15 19:39:10 +0530

    first Commit

---
+++ resources/views/website/partials/head.blade.php
@@ -0,0 +1,32 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- ===============================================-->
+<!--    Document Title-->
+<!-- ===============================================-->
+<title>Aranyak | Landing Page Template</title>
+
+<!-- ===============================================-->
+<!--    Favicons-->
+<!-- ===============================================-->
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/img/favicons/apple-touch-icon.png') }}?v={{ time() }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/img/favicons/favicon-32x32.png') }}?v={{ time() }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/img/favicons/favicon-16x16.png') }}?v={{ time() }}">
+<link rel="shortcut icon" type="image/x-icon" href="{{ asset('assets/img/favicons/favicon.ico') }}?v={{ time() }}">
+<link rel="manifest" href="{{ asset('assets/img/favicons/manifest.json') }}?v={{ time() }}">
+<meta name="msapplication-TileImage" content="{{ asset('assets/img/favicons/mstile-150x150.png') }}?v={{ time() }}">
+<meta name="theme-color" content="#ffffff">
+<script src="{{ asset('assets/js/config.js') }}?v={{ time() }}"></script>
+
+<!-- ===============================================-->
+<!--    Stylesheets-->
+<!-- ===============================================-->
+<link rel="stylesheet" href="{{ asset('vendors/swiper/swiper-bundle.min.css') }}?v={{ time() }}">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;600;700&amp;display=swap" rel="stylesheet">
+<link href="{{ asset('assets/css/theme.css') }}?v={{ time() }}" rel="stylesheet" id="style-default">
+<link href="{{ asset('assets/css/user-rtl.css') }}?v={{ time() }}" rel="stylesheet" id="user-style-rtl">
+<link href="{{ asset('assets/css/user.css') }}?v={{ time() }}" rel="stylesheet" id="user-style-default">
+<link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
---
+++ resources/views/website/partials/js.blade.php
@@ -0,0 +1,12 @@
+<!-- ===============================================-->
+<!--    JavaScripts-->
+<!-- ===============================================-->
+<script src="{{ asset('vendors/popper/popper.min.js') }}?v={{ time() }}"></script>
+<script src="{{ asset('vendors/bootstrap/bootstrap.min.js') }}?v={{ time() }}"></script>
+<script src="{{ asset('vendors/is/is.min.js') }}?v={{ time() }}"></script>
+<script src="{{ asset('vendors/countup/countUp.umd.js') }}?v={{ time() }}"></script>
+<script src="{{ asset('vendors/swiper/swiper-bundle.min.js') }}?v={{ time() }}"></script>
+<script src="{{ asset('vendors/lodash/lodash.min.js') }}?v={{ time() }}"></script>
+<script src="https://polyfill.io/v3/polyfill.min.js?features=window.scroll"></script>
+<script src="{{ asset('vendors/list.js/list.min.js') }}?v={{ time() }}"></script>
+<script src="{{ asset('assets/js/theme.js') }}?v={{ time() }}"></script>
